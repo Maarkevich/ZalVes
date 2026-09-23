759cbf8469462c8dfeee95a03e3a3a69e013818e
Maarkevich <markevich.andrey96@gmail.com>
2026-09-24 00:19:08 +0300

    Add files via upload

---
+++ exercise-catalog.js
@@ -0,0 +1,99 @@
+window.ZALVES_EXERCISES = [
+  {
+    id: 'rowing-machine',
+    image: 'exercise-01.png',
+    name: 'ГРЕБЛЯ НА ТРЕНАЖЁРЕ',
+    muscles: [
+      'Широчайшие мышцы спины',
+      'Трапециевидные мышцы',
+      'Ромбовидные мышцы',
+      'Бицепсы рук',
+      'Мышцы ног (квадрицепсы, задняя поверхность бедра, икроножные)',
+      'Ягодичные мышцы'
+    ]
+  },
+  {
+    id: 'pec-deck',
+    image: 'exercise-02.png',
+    name: 'СВЕДЕНИЕ РУК В ТРЕНАЖЁРЕ (БАТТЕРФЛЯЙ)',
+    muscles: ['Большие грудные мышцы']
+  },
+  {
+    id: 'seated-shoulder-press',
+    image: 'exercise-03.png',
+    name: 'ЖИМ ОТ ПЛЕЧ СИДЯ',
+    muscles: ['Плечи']
+  },
+  {
+    id: 'seated-chest-press',
+    image: 'exercise-04.png',
+    name: 'ЖИМ ОТ ГРУДИ СИДЯ',
+    muscles: ['Большие грудные мышцы']
+  },
+  {
+    id: 'front-arm-fly',
+    image: 'exercise-05.png',
+    name: 'СВЕДЕНИЕ РУК ПЕРЕД СОБОЙ',
+    muscles: ['Задняя дельта']
+  },
+  {
+    id: 'upright-shrug',
+    image: 'exercise-06.png',
+    name: 'ПОДНЯТИЕ ЛОКТЕЙ ВВЕРХ',
+    muscles: ['Трапециевидные мышцы (верхняя часть)']
+  },
+  {
+    id: 'glute-bridge',
+    image: 'exercise-07.png',
+    name: 'ЯГОДИЧНЫЙ МОСТИК',
+    muscles: [
+      'Ягодичные мышцы (основная нагрузка)',
+      'Задняя поверхность бедра (бицепс бедра)',
+      'Разгибатели спины (стабилизация)'
+    ]
+  },
+  {
+    id: 'leg-press-45',
+    image: 'exercise-08.png',
+    name: 'ЖИМ НОГАМИ ПОД УГЛОМ 45°',
+    muscles: [
+      'Четырёхглавая мышца бедра (квадрицепсы)',
+      'Ягодичные мышцы',
+      'Двуглавая мышца бедра (бицепс бедра)'
+    ]
+  },
+  {
+    id: 'barbell-bench-press',
+    image: 'exercise-09.png',
+    name: 'ЖИМ ШТАНГИ ЛЁЖА',
+    muscles: ['Грудные мышцы', 'Трицепс', 'Передние дельты (плечи)']
+  },
+  {
+    id: 'preacher-curl',
+    image: 'exercise-10.png',
+    name: 'СГИБАНИЕ РУК НА СКАМЬЕ СКОТТА',
+    muscles: ['Бицепс']
+  },
+  {
+    id: 'seated-leg-adduction',
+    image: 'exercise-11.png',
+    name: 'СВЕДЕНИЕ НОГ СИДЯ',
+    muscles: ['Приводящие мышцы бедра (внутренняя поверхность бедра)']
+  },
+  {
+    id: 'seated-leg-curl',
+    image: 'exercise-12.png',
+    name: 'СГИБАНИЕ НОГ СИДЯ',
+    muscles: ['Задняя поверхность бедра']
+  },
+  {
+    id: 'lat-pulldown',
+    image: 'exercise-13.png',
+    name: 'ТЯГА ВЕРХНЕГО БЛОКА',
+    muscles: [
+      'Широчайшие мышцы спины',
+      'Трапециевидные мышцы',
+      'Задние дельтовидные мышцы'
+    ]
+  }
+];
